dffc9412d8457a91e323db20e2beae0b9b98305a
Henry Catalini Smith <henry@catalinismith.se>
2025-01-06 00:03:30 +0100

    Add rehype-format

--- eleventy.config.ts
+++ eleventy.config.ts
@@ -1,3 +1,7 @@
+import EleventyUnifiedPlugin from "eleventy-plugin-unified";
+
 export default async function (eleventyConfig) {
-  //
+  eleventyConfig.addPlugin(EleventyUnifiedPlugin, {
+    htmlTransforms: [["rehype-format", { indent: " " }]],
+  });
 }
