372ce6be8df4b70f02a8cd71b92d7b868acfa216
Henry Catalini Smith <henry@catalinismith.se>
2025-01-23 16:33:28 +0100

    Remove basePath

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH
-    ? process.env.NEXT_PUBLIC_BASE_PATH
-    : "",
   output: "export",
 };
 
