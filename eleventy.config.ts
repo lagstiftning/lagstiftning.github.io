27436252359ac6abf8d7426ef96bfff524e0ab53
Henry Catalini Smith <henry@catalinismith.com>
2025-01-23 16:05:43 +0100

    Clean slate

--- eleventy.config.ts
+++
@@ -1,7 +0,0 @@
-import EleventyUnifiedPlugin from "eleventy-plugin-unified";
-
-export default async function (eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyUnifiedPlugin, {
-    htmlTransforms: [["rehype-format", { indent: " " }]],
-  });
-}
